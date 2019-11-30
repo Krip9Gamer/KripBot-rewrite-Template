a0d605b592c0b7792626d8014e7e5e86e5429697
Krip9Gamer <58361493+Krip9Gamer@users.noreply.github.com>
2019-11-30 19:31:28 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,51 @@
+import discord 
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print("Bot is online.")
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+@client.command()
+async def clear(ctx, amount=1000):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def ping(ctx):
+     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ['It is Certain.',
+                 'It is decidedly so..',
+                 'Without a doubt.',
+                 'Yes.',
+                 'You may rely on it.',
+                 'As i see it, yes.',
+                 'Most likely.',
+                 'Outlook good',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not tell..',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 "Don't count on it.",
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good..',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+client.run('YOUR TOKEN HERE')
+
